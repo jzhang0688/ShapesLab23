cb080ad0522638315b7a226b6678df4a53a2c65b
BT_1N3_19 <fwilking4736@bths.edu>
2018-10-18 08:38:18 -0400

    ;)))

--- src/ShapesLab23/Main.java
+++ src/ShapesLab23/Main.java
@@ -3,6 +3,17 @@ package ShapesLab23;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Shape sape1 = new rectangle(10,5);
+        Shape shape2 = new ETriangle(5);
+        Shape shape3 = new Circle(10);
+        Shape shape4 = new RPentagon(5);
+
+        Shape[] shapes = {shape1,shape2,shape3,shape4};
+
+        for(Shape cur : shapes){
+            System.out.println(cur);
+            System.out.println("My area is: " + cur.getArea());
+            System.out.println("My perimeter is:" + cur.getPerimeter());
+        }
     }
 }
